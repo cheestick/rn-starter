97c960fa69a82ee728584d98575ec04602b47039
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-07-01 00:59:09 +0300

    theme configuration started

---
+++ theme/index.ts
@@ -0,0 +1,30 @@
+const colors = {
+  white: "hsla(0, 0%, 100%, 1)",
+  black: "hsla(0, 0%, 0%, 1)",
+  primary: {},
+  secondary: {},
+  accent: {},
+  disabled: {},
+};
+
+const text = {
+  size: {
+    sm: 16,
+    md: 20,
+    lg: 24,
+    xl: 30,
+    body: 18,
+    heading: 28,
+  },
+};
+
+const utils = {
+  width: 2,
+  radius: 10,
+};
+
+export const theme = {
+  colors,
+  text,
+  utils,
+} as const;
