86da64af50453718f7218809ab7f1f14c96da30a
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-07-01 21:34:22 +0300

    navigation

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 import { sx } from "./home.styles";
 import { sxg } from "../theme/global";
 
-const Home = () => {
+// FIXME: any type
+const Home = ({ navigation }: any) => {
+  const pressHandler = () => {
+    navigation.navigate("ReviewDetails");
+    // navigation.push("ReviewDetails");
+  };
+
   return (
     <View style={[sxg.container, sx.container]}>
       <Text style={[sxg.titleText, sx.titleText]}>Home Screen</Text>
+      <Button title="Review Details" onPress={pressHandler} />
     </View>
   );
 };
--- src/screens/reviewDetails.tsx
+++ src/screens/reviewDetails.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 import { sx } from "./reviewDetails.styles";
 import { sxg } from "../theme/global";
 
-const ReviewDetails = () => {
+// FIXME: any type
+const ReviewDetails = ({ navigation }: any) => {
+  const pressHandler = () => {
+    navigation.goBack();
+  };
+
   return (
     <View style={[sxg.container, sx.container]}>
       <Text style={[sxg.titleText, sx.text]}>ReviewDetails Screen</Text>
+      <Button title="Back to Home" onPress={pressHandler} />
     </View>
   );
 };
